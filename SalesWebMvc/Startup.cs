e2d648f301315651d9abf4137ca3691070ace632
Anderson Cardozo de Souza <devsouzacardozo@gmail.com>
2021-04-04 22:07:45 -0300

    install - Pomelo.EntityFrameworkCore.Mysql -vesion 3.1.8

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -34,7 +34,9 @@ namespace SalesWebMvc {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
     services.AddDbContext<SalesWebMvcContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
+            options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), BuilderExtensions => 
+                 BuilderExtensions.MigrationsAssembly("SalesWebMvc")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
